eed4bb8bb4de32f096fb87b08dd21c3998f719d9
thisiscindy24 <cindyannwu@gmail.com>
2025-02-06 03:59:40 +0000

    test

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get ("/howdy") do
+  "hello"
+  erb (:hello)
+end
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:new_square_cal)
+  
+end
+
+get ("/square/new") do  
+  erb(:new_square_cal)
+end
+
+get ("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num ** 2
+  erb(:square_results)
 end
