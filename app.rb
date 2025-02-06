a2c360d4247257128dd5abbd819af497e58da81d
thisiscindy24 <cindyannwu@gmail.com>
2025-02-06 04:16:19 +0000

    square root

--- app.rb
+++ app.rb
@@ -20,3 +20,13 @@ get ("/square/results") do
   @the_result = @the_num ** 2
   erb(:square_results)
 end
+
+get ("/square_root/new") do
+  erb(:square_root_new)
+end
+
+get ("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_result = Math.sqrt(@the_num)
+  erb(:square_root_results)
+end
