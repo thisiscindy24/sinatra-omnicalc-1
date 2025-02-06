af837f34c5d23844fa6b529af516c5a559a35152
thisiscindy24 <cindyannwu@gmail.com>
2025-02-06 05:44:02 +0000

    random number

--- app.rb
+++ app.rb
@@ -30,3 +30,35 @@ get ("/square_root/results") do
   @the_result = Math.sqrt(@the_num)
   erb(:square_root_results)
 end
+
+get ("/payment/new") do
+  erb(:payment_new)
+end
+
+get ("/payment/results") do
+  @apr_user = (params.fetch("user_apr", "0").to_f / 100).to_fs(:percentage)
+  @apr = params.fetch("user_apr").to_f / 100 / 12
+  @user_years_user = params.fetch("user_years").to_i
+  @user_years = params.fetch("user_years").to_i * 12
+  @user_pv= params.fetch("user_pv").to_f.to_fs(:currency)
+  @the_numerator = @apr * @user_pv
+  @the_dem = 1 - (1+@apr)**(-@user_years)
+  @result = @the_numerator/@the_dem
+  erb(:payment_results)
+end
+
+get ("/random/new") do
+  erb(:random_new)
+end
+
+get ("/random/results") do
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+
+  if @min < @max
+    @result = rand(@min..@max)
+  else
+    @result = ""
+  end
+  erb(:random_results)
+end
